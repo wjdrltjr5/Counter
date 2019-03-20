619f95312f954b36dbe4e4cb9a4023b08a69deea
dit08 <dit@imac-3.local>
2019-03-20 16:38:37 +0900

    버튼을 누를시 숫자가 1씩 증가하고 최대 10까지 증가후 다시 0으로 되돌아감

--- counter01/ViewController.swift
+++ counter01/ViewController.swift
@@ -9,12 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var count = 0
+    
+    @IBAction func bttap(_ sender: Any) {
+        if count < 10{
+            count = count + 1
+            print(count)
+            show1.text = String(count)
+        }
+        if count == 10 {
+            count = 0
+            print(count)
+            show1.text = String(count)
+        }
+    }
+    
+    @IBOutlet weak var show1: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        show1.text="0"
+    
+    
+    
     }
-
 
 }
 
